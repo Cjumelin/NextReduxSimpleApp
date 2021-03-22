3ef38eca9e3ab5e99e983c3e9632f36798efd55a
Jim <clement.jumelin@gmail.com>
2021-03-22 20:07:24 +0100

    Align helloworld text

--- pages/helloworld.tsx
+++ pages/helloworld.tsx
@@ -1,7 +1,9 @@
 export default function Helloworld() {
     return (
-        <div>
-            Hello World
+        <div className={"min-h-screen min-w-full flex flex-col justify-center align-center"}>
+            <p className={"text-center"}>
+                Hello World
+            </p>
         </div>
     )
 }
